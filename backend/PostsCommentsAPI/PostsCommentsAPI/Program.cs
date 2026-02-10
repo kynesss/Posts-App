6361d22cb24abee786ae253cb63b77e523df39cd
Kamil Piróg <kamilpirog98@gmail.com>
2026-02-10 14:23:41 +0100

    Initial project setup: .NET + React + Docker

---
+++ backend/PostsCommentsAPI/PostsCommentsAPI/Program.cs
@@ -0,0 +1,27 @@
+using MediatR;
+using FluentValidation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
